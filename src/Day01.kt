7c89841612dd8476a149e8e61b6e4ac893f5f5f4
Arthur Embleton <arthur@blerg.net>
2024-12-01 17:01:28 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val rows = input.map { it.split("   ") }
+        val left = rows.map { it[0].toInt() }.sorted()
+        val right = rows.map { it[1].toInt() }.sorted()
+        return left.zip(right).sumOf { if (it.first < it.second) it.second-it.first else it.first-it.second }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rows = input.map { it.split("   ") }
+        val left = rows.map { it[0].toInt() }.sorted()
+        val right = rows.map { it[1].toInt() }.sorted()
+        return left.sumOf { l -> right.count { it == l } * l }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(readInput("d1p1")).println()
+    part2(readInput("d1p2")).println()
 }
